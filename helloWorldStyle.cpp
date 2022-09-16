2bd68ef7de5ef9b3b114fb2bdfdab0ef9925ed1f
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-09-16 02:25:13 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Peter Sobowale All rights reserved.
+// .
+// Created by: Peter Sobowale
+// Date: Sept, 15 2022
+// This program displays "Hello, World with style".
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World with style!" << std::endl;
+    std::cout << "C++ i a bit more complicated than Python." << std::endl;
+}
